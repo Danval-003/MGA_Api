a58362c91e9370e1854da1c52cb371fb0c977c1b
danar <danarvare2003@gmail.com>
2023-05-09 16:49:43 -0600

    Arreglo bug

--- documentation/documentation.py
+++ documentation/documentation.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 doc_bp = Blueprint('documentation', __name__)
 
 
-@doc_bp.route('/galeriasDoc', methods=['GET'])
+@doc_bp.route('/galerasDoc', methods=['GET'])
 def docGalerias():
-    return render_template('galeriasDoc.html')
+    return render_template('galerasDoc.html')
