05dbf5606613b9c1a3ce41dec15bae8482d16646
danar <danarvare2003@gmail.com>
2023-05-10 11:06:46 -0600

    Arreglo bug

--- routers/registerRoute.py
+++ routers/registerRoute.py
@@ -8,7 +8,7 @@ register_bp = Blueprint('register', __name__)
 def makeRegister():
     res = request.get_json()
     parameters = ['cantidadAlimento', 'decesos', 'observaciones', 'idGalera', 'pesado']
-    if 'cantidadAlimento' not in res or 'decesos' not in res or 'observaciones' not in res or 'idGalera' not in res or 'pesado' in res:
+    if 'cantidadAlimento' not in res or 'decesos' not in res or 'observaciones' not in res or 'idGalera' not in res or 'pesado' not in res:
         return make_response(jsonify(
             {'message': 'Error al colocar los datos debias mandar como parametros num_galera y num_lote'}, 404))
 
