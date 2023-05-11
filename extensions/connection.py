bbf741838c675c6a6192b19c3d484312a959ee2e
danar <danarvare2003@gmail.com>
2023-05-10 19:13:25 -0600

    Arreglo bug

--- extensions/connection.py
+++ extensions/connection.py
@@ -2,10 +2,10 @@ import psycopg2
 
 
 # Funcion que realiza la coneccion a la base de datos
-def connect(user='postgres', password='123456'):
+def connect(user='postgres', password='OrtalizasDelUerto22'):
     # Se le manda la informacion para realizar la coneccion
     conn = psycopg2.connect(
-        host="localhost",
+        host="database-1.c5xesezrpxni.us-east-2.rds.amazonaws.com",
         database="postgres",
         user=user,
         password=password
