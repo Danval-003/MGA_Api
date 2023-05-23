a41a2b1b439e3938cb4473283a83eeda2d016f11
danar <danarvare2003@gmail.com>
2023-05-23 12:41:15 -0600

    Se agrego la ruta para obtener registros por fecha

--- routers/registerRoute.py
+++ routers/registerRoute.py
@@ -27,13 +27,13 @@ def obtainRegisterDate():
          from registro inner join galera g on g.id_galera = registro.id_galera"""
 
     if 'date' in res or 'idTrabajador' in res or 'idLote' in res:
-        query = query + ' where '
+        query = query + ' where'
         if 'date' in res:
-            query = query + "fecha > '" + res['date'] + " 0:0:0.0' and  fecha < '" + res['date'] + " 23:59:59.0' and"
+            query = query + " fecha > '" + res['date'] + " 0:0:0.0' and  fecha < '" + res['date'] + " 23:59:59.0' and"
         if 'idTrabajador' in res:
-            query = query + "id_trabajador = '%s' and" % (res['idTrabajador'],)
+            query = query + " id_trabajador = '%s' and" % (res['idTrabajador'],)
         if 'idLote' in res:
-            query = query + "id_lote = '%s' and" % (res['idTrabajador'],)
+            query = query + " id_lote = '%s' and" % (res['idTrabajador'],)
         query = query[:-3]
     return obtainRegisters(query)
 
