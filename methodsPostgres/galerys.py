852ca747e08dc6991b8c45927048cb3c6641d2d2
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-07-20 18:41:31 -0600

    Inicio de programas nuevos

--- methodsPostgres/galerys.py
+++ methodsPostgres/galerys.py
@@ -67,3 +67,24 @@ def infoGalera(tupleValues):
         status['error'] = 404
 
     return make_response(jsonify(status), status['error'])
+
+def createGaleras(id_lote):
+    status = {
+        'error': 202,
+        'message': '',
+        'data': []
+    }
+    try:
+        conn = connect()
+        cur = conn.cursor()
+        cur.execute('''
+        select * from get_galeries(%s);
+            ''', (id_lote,))
+        cur.fetchall()
+        
+        status['message'] = 'Good Job'
+    except psycopg2.Error as e:
+        status['message'] = str(e)
+        status['error'] = 404
+
+    return make_response(jsonify(status), status['error'])
---
+++ methodsPostgres/workers.py
@@ -0,0 +1,5 @@
+import psycopg2
+from flask import jsonify, make_response
+
+from extensions.connection import connect
+
