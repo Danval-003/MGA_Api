f856fa69af1505a8827badd1682b3c11a30c3acb
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2023-11-20 21:32:08 -0600

    Ruta final

--- methodsPostgres/lotes.py
+++ methodsPostgres/lotes.py
@@ -30,3 +30,30 @@ def obtainLote(tupleValues):
 
     return make_response(jsonify(status), status['error'])
 
+
+def obtainLotes():
+    status = {
+        'error': 202,
+        'message': '',
+        'data': []
+    }
+    try:
+        conn = connect()
+        cur = conn.cursor()
+        cur.execute('''select distinct(id_lote) from galeras_info inner join galeras g on g.id_galera = galeras_info.id_galera ''')
+        rows = cur.fetchall()
+
+        status['data'] = [
+            {
+                'idLote': row[0]
+            }
+            for row in rows
+        ]
+        status['message'] = 'Good Job'
+    except psycopg2.Error as e:
+        status['message'] = str(e)
+        status['error'] = 404
+
+    return make_response(jsonify(status), status['error'])
+
+
--- routers/lotes.py
+++ routers/lotes.py
@@ -8,16 +8,9 @@ from methodsPostgres.lotes import *
 lotes_bp = Blueprint('lotes', __name__)
 
 
-@lotes_bp.route('/lObtain', methods=['POST'])
+@lotes_bp.route('/lObtain', methods=['GET'])
 def obtainLotes():
-    res = request.get_json()
-    if 'dpiTrabajador' not in res:
-        return make_response(jsonify(
-            {'message': 'Error al colocar los datos debias mandar como parametros dpiTrabajador'}, 404))
-
-    infoTupla = tuple([res['dpiTrabajador']])
-
-    return obtainLote(infoTupla)
+    return obtainLotes()
 
 
 @lotes_bp.route('/loteObtain', methods=['GET'])
